5448d9916d0e37e8603067f10e128dde609d135f
manuelJung <manuel.jung.wwi12@gmail.com>
2024-07-05 13:48:56 +0200

    setup example + turbo

---
+++ example/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+import { DemoComponent } from 'grid-editor'
+
+function App() {
+
+  return (
+    <DemoComponent label='foo'/>
+  )
+}
+
+export default App
--- packages/grid-editor/README.md
+++ packages/grid-editor/README.md
@@ -1,3 +1,6 @@
 ## Setup
 
 https://dev.to/receter/how-to-create-a-react-component-library-using-vites-library-mode-4lma
+
+// component-editor
+// component-viewer
